172896721b545c87a50fbde033464631944e6130
Bob Taylor <cameradv@gmail.com>
2022-07-18 11:17:56 -0700

    test programs

---
+++ toggletest.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime
+
+DOOR_RELAY = 4
+REED_SWITCH = 14
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)  # the pin numbers refer to the board connector not the chip
+GPIO.setwarnings(False)
+GPIO.setup(REED_SWITCH, GPIO.IN, GPIO.PUD_UP) 
+GPIO.setup(DOOR_RELAY, GPIO.OUT)
+GPIO.output(DOOR_RELAY, GPIO.HIGH)
+
+
+def Garage():
+                print ("Pressing button")
+                GPIO.output(DOOR_RELAY, GPIO.LOW)
+                time.sleep(1)
+                GPIO.output(DOOR_RELAY, GPIO.HIGH)
+                time.sleep(8)
+
+                if GPIO.input(REED_SWITCH) == GPIO.LOW:
+                      print ("Garage is Closed")
+                else:
+                      print ("Garage is Open")
+
+if __name__ == '__main__':
+	Garage()
